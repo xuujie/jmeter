4842feb9648fe4c6cc76bbbfb969f912d6afb16b
xujie <xuujie@gmail.com>
2019-12-06 16:31:19 +0800

    give server time to warm up

--- grafana/deploy.sh
+++ grafana/deploy.sh
@@ -1,6 +1,7 @@
 kubectl apply -f k8s.yml
 
-sleep 20
+echo Wait for 10 seconds
+sleep 10
 
 #create api key
 #curl -X POST http://admin:JmeterGrafana123@localhost:3000/api/auth/keys -H "Content-Type: application/json" -d "{\"name\": \"clikey\", \"role\": \"Admin\"}"
--- influxdb/deploy.sh
+++ influxdb/deploy.sh
@@ -1,7 +1,8 @@
 #init k8s
 kubectl apply -f k8s.yml
 
-sleep 20
+echo Wait for 10 seconds
+sleep 10
 
 #create database jmeter
 curl -X POST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE jmeter"
